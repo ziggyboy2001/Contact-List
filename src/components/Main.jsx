92d12e0e5165e113ca2c1ac223bc33501a84abeb
ziggyboy2001 <brentpurks1@icloud.com>
2023-02-15 10:57:46 -0600

    steps one through three

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
+import dummyContacts from "../dummyData";
 
 const Main = () => {
+  const [contacts, setContacts] = useState(dummyContacts);
+  {
+    contacts.map((contact) => {
+      return contact.name;
+    });
+  }
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Email</th>
+            </tr>
+            {contacts.map(contact => {
+
+              return (
+                <tr key={contact.id}>
+                  <td>{contact.name}</td>
+                  <td>{contact.phone}</td>
+                  <td>{contact.email}</td>
+                </tr>
+                
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
+  
 };
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Main = () => {
-  return (
-    <div id="main">
-      <div id="navbar">
-        <div>Contact List</div>
-      </div>
-      <div id="container">
-        {/* your components here */}
-      </div>
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <Main />,
-  document.getElementById('app')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "./components";
+ReactDOM.render(<Main />, document.getElementById("app"));
